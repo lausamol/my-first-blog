6d47214f201b904a2d8f404c8d808ce71792d844
Laura Sampedro <lausamol@gmail.com>
2018-11-17 16:07:49 +0100

    modificacion de allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*kief^^#za2y&xr4n2-rd#pk0&$ho_59b%in8$9c)ifs(wa0vq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
